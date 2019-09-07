77379e59c5d4da8acb3d538b0c845c73685dc99a
David Servias <davidservias@gmail.com>
2019-09-06 17:21:12 -0700

    Add files via upload
    Add reply to mentions function, and error handling

--- my_twitter_friend.py
+++ my_twitter_friend.py
@@ -1,6 +1,7 @@
 import tweepy
 import time
 import json
+import random
 from configBot import create_api
 
 api = create_api()
@@ -11,8 +12,6 @@ def get_data():
     with open("my_twitter_friend_data.json", "r") as read_file:
         received_data = json.load(read_file)
         return received_data
-
-
 data = get_data()
 
 
@@ -28,100 +27,91 @@ def search_tweets(string):
 
 
 def follow_new_followers(api):
-    for follower in tweepy.Cursor(api.followers).items():
+    for follower in api.followers():
         if not follower.following:
-            print("following " + follower.name)
-            follower.follow()
+            try:
+                follower.follow()
+                print("following " + follower.name)
+            except tweepy.TweepError:
+                print("Cannot follow " + follower.name + ". (The follow request my be pending.)")
+                continue
 
 def like_friends_tweets2():
     followers = api.followers()
     last_favorite = data["last_favorite"]
     for follower in followers:
-        recent_tweets = api.user_timeline(follower.id, since_id=last_favorite)
-        if recent_tweets:
-            for recent_tweet in recent_tweets:
-                print(str(recent_tweet.id) + " " + recent_tweet.text)
-                if not recent_tweet.favorited:
-                    recent_tweet.favorite()
-                if recent_tweet.id > last_favorite:
-                    last_favorite = recent_tweet.id
-    data["last_favorite"] = last_favorite
-    update_data(data)
-
-
-def like_followers_tweets():
-    # possibly should delete this function (like_friends_tweets2 is an improvement on this)
-    # filters out tweets that have already been liked
-    # likes the tweets that haven't been liked yet
-    # updates last_favorite in data
-    last_favorite = data["last_favorite"]
-    latest_tweets = api.home_timeline(since_id=last_favorite)
-    latest_tweets.reverse()
-    print("searching for recent tweets")
-    if latest_tweets:
-        print("liking...")
-        for tweet in latest_tweets:
-            print(str(tweet.id) + " " + tweet.text)
-            if tweet.user.id != self.id and tweet.id > data["last_favorite"]:
-                time.sleep(15)
-                print(str(tweet.id) + " " + tweet.text)
-                tweet.favorite()
-                last_favorite = tweet.id
-    data["last_favorite"] = last_favorite
-    update_data(data)
-
-
-def get_mentions(last_mention_id):
-    # needs further testing
-    mentions = tweepy.Cursor(api.mentions_timeline, since_id=last_mention_id).items()
-    for tweet in mentions:
-        print(tweet.user + ": " + tweet.text)
-        new_last_mention_id = tweet.id
-        data["last_mention_id"] = new_last_mention_id
-
-
-def check_mentions(api, keywords, last_mention_id):
-    #needs further testing
-    new_last_mention_id = last_mention_id
-    for tweet in tweepy.Cursor(api.mentions_timeline, since_id=last_mention_id).items():
-        new_since_id = max(tweet.id, last_mention_id)
-        if tweet.in_reply_to_status_id is not None:
+        try:
+            recent_tweets = api.user_timeline(follower.id, since_id=last_favorite)
+            if recent_tweets:
+                for recent_tweet in recent_tweets:
+                    print(str(recent_tweet.id) + " " + recent_tweet.text)
+                    if not recent_tweet.favorited:
+                        recent_tweet.favorite()
+                    if recent_tweet.id > last_favorite:
+                        last_favorite = recent_tweet.id
+        except tweepy.TweepError:
             continue
-        if any(keyword in tweet.text.lower() for keyword in keywords):
-            logger.info(f"Answering to {tweet.user.name}")
-
-            if not tweet.user.following:
-                tweet.user.follow()
-
-            api.update_status(
-                status="Please reach us via DM",
-                in_reply_to_status_id=tweet.id,
-            )
-    return new_since_id
+    data["last_favorite"] = last_favorite
+    update_data(data)
 
 
-# like_followers_tweets()
-# time.sleep(5)
-# follow_new_followers(api)
+def reply_to_mentions():
+    # needs further testing
+    last_reply = data["last_reply"]
+    mentions = api.mentions_timeline(since_id = last_reply)
+    mentions.reverse()
+    for mention in mentions:
+        print(str(mention.id) + ": " + mention.text)
+        screen_name = "@" + mention.user.screen_name
+        reply_text = screen_name + " " + choose_random_reply()
+        print(mention.user.name)
+        print(mention.text)
+        print("reply: " + reply_text)
+        try:
+            api.update_status(status = reply_text, in_reply_to_status_id = mention.id, )
+            if not data["last_reply"] > mention.id:
+                data["last_reply"] = mention.id
+            update_data(data)
+            time.sleep(15)
+        except tweepy.TweepError:
+            print("Reply failed. Not sure why. Hope that helps")
+            continue
+
+def choose_random_reply():
+    reply = ""
+    replies = data["replies"]
+    num_choices = len(replies)
+    index = random.randint(0, num_choices-1)
+    reply = replies[index]
+    return reply
 
 
 def run_process():
     while True:
         try:
-            print("searching tweets")
+            print("searching for mentions")
+            reply_to_mentions()
+            print("waiting")
+            time.sleep(60)
+            print("searching tweets from followers")
             like_friends_tweets2()
             print("waiting")
             time.sleep(60)
-            # print(api.rate_limit_status())
-            # time.sleep(30)
             print("searching for new followers")
             follow_new_followers(api)
             time.sleep(60)
+            print("waiting")
         except tweepy.TweepError:
             print("error")
             error_tweet = "I think something went wrong. I may not like any tweets "
-            error_tweet.append(" for awhile until I can figure this out.")
+            error_tweet += " for awhile until I can figure this out."
             api.update_status(error_tweet)
 
 
+# follow_new_followers(api)
+# like_friends_tweets2()
+# reply_to_mentions()
 run_process()
+
+
+
---
+++ write_to_data.py
@@ -0,0 +1,40 @@
+import json
+
+def get_data():
+    with open("my_twitter_friend_data.json", "r") as read_file:
+        received_data = json.load(read_file)
+        return received_data
+
+def update_data(updated_data):
+    with open("my_twitter_friend_data.json", "w") as write_file:
+        json.dump(updated_data, write_file)
+
+data = get_data()
+print(json.dumps(data, sort_keys=True, indent=4))
+
+
+# alter data here
+data["compliments"] = "empty"
+data["encouragement"] = "empty"
+data["fun_stories"] = "empty"
+
+data["replies"] = [
+    "Hey Buddy!!!",
+    "What's up, friend!!!",
+    "I miss you, pal!!!",
+    "High five!!!",
+    "You get down with your bad self!!!",
+    "Boom, roasted!",
+    "Yeet!",
+    "I know, right!",
+    "I heard that!",
+    "That's how it's done!!!",
+    "Ain't that the truth!",
+    "This is AWESOME!",
+    "I live for your tweets!!!",
+    "Better than a jab in the eye...!",
+    
+
+]
+# ######################################################
+update_data(data)
